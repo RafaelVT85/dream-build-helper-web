1f7d2d86e4abc121ac940515948f46a7df1b2a13
Rafael Valenca <valenca.rafa@gmail.com>
2026-09-26 18:34:40 -0300

    Add /admin route link in header

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import GamesList from "./routes/GamesList";
 import GameDetail from "./routes/GameDetail";
 import BuildDetail from "./routes/BuildDetail";
+import AdminPage from "./admin/AdminPage";
 
 export default function App() {
   return (
@@ -10,12 +11,16 @@ export default function App() {
         <Link to="/" className="app-title">
           Ajuda Jogos
         </Link>
+        <Link to="/admin" className="pill" style={{ textDecoration: "none" }}>
+          Admin
+        </Link>
       </header>
       <main>
         <Routes>
           <Route path="/" element={<GamesList />} />
           <Route path="/jogos/:gameId" element={<GameDetail />} />
           <Route path="/jogos/:gameId/builds/:buildId" element={<BuildDetail />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </main>
     </div>
